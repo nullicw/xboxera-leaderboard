cabd966da6521c1ddd84cdc89f25d75529525e5e
nullicw <matthias.schmidt.1@gmx.de>
2021-04-23 20:22:44 +0200

    changed input to csv and added extra output for discourse
    and small refactorings of formats for better readability

--- Program.cs
+++ Program.cs
@@ -17,6 +17,11 @@ namespace XboxeraLeaderboard
     /// Takes a | separated list of gamertags (with their xuid and last weeks gamerscore)
     /// polls the open XBL api for the current gamerscore and outputs the (weekly) leaderboard
     /// in a table format Discourse understands.
+    /// the programm writes 2 files:
+    /// - the first includes XUIDs and is to be used as the input for the next weekly run. it is
+    ///   also in a csv-format Excel understands.
+    /// - the second omits XUIDs and is Discourse table format. this file is optional, if its name
+    ///   is missing on the commandline the program writes the output to stdout.
     /// </summary>
     /// <remarks>
     /// get xuids for gamertag: https://www.cxkes.me/xbox/xuid
@@ -24,60 +29,88 @@ namespace XboxeraLeaderboard
     /// </remarks>
     public class Program
     {
+        private const char               CsvSeparator       = ';';
+        private static readonly string[] CsvHeader          = new string[] { "#;User;Gamertag;XUID;Initial GS;Final GS;Gains;Points" };
+        private static readonly string[] DiscourseHeader    = new string[] {
+                                                                 "|#|User|Gamertag|XUID|Initial GS|Final GS|Gains|Points|",
+                                                                 "| --- | --- | --- | --- | --- | --- | --- | --- |"
+                                                              };
+
+        /// <summary>
+        /// the API key for openXBL needed to call their REST services (a new can be generated on their
+        /// site on demand at no cost)
+        /// </summary>
         private const string OpenXBLKey = "koso444og0w8w8ckw8s44sgk0gkwwos4g8s";
 
         public static async Task Main(string[] args)
         {
-            if(args.Length != 2)
+            if(args.Length < 2 || args.Length > 3)
             {
                 Console.WriteLine("usage:");
-                Console.WriteLine("XboxeraLeaderboard.exe inputfilename outputfilename");
-                Console.WriteLine("like XboxeraLeaderboard.exe lastweek.txt thisweek.txt");
+                Console.WriteLine("XboxeraLeaderboard.exe $lastweek-filename $nextweek-filename ($output-for-xboxera)");
+                Console.WriteLine("i.e. XboxeraLeaderboard.exe week31.csv week32.csv");
             }
             else
             {
                 var input = await File.ReadAllLinesAsync(args[0]);
 
-                // getting new gamerscores from xbox
+                // parsing input file
 
-                var newScores = input.Where(l => !string.IsNullOrWhiteSpace(l))
-                                     .Skip(2) // headlines
-                                     .Select(l => l.Split('|', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
-                                     .Select(l => new { user = l[1], gamerTag = l[2], xuid = long.Parse(l[3]), lastScore = long.Parse(l[5]) })
-                                     .Select(g => new { g, newScore = ReadCurrentGamerScore(g.gamerTag, g.xuid).Result })
+                var users = input.Where(l => !string.IsNullOrWhiteSpace(l))
+                                 .Skip(1) // headline
+                                 .Select(l => l.Split(CsvSeparator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+                                 .Select(l => new { user = l[1], gamerTag = l[2], xuid = long.Parse(l[3]), lastScore = long.Parse(l[5]) });
+
+                // getting current gamerscores from xbox
+
+                var newScores = users.Select(g => new { g, newScore = ReadCurrentGamerScore(g.gamerTag, g.xuid).Result })
                                      .Select(n => new { n.g.user, n.g.gamerTag, n.g.xuid, n.g.lastScore, n.newScore, gains = Gains(n.g.lastScore, n.newScore) })
                                      .OrderByDescending(n => n.gains);
 
-                // ranking (take into account users with same gains have to be ranked the same!)
+                // ranking (users with same gains have to be ranked the same!)
 
                 var ranked = newScores.OrderByDescending(s => s.gains)
-                                      .Select((s, i) => new { rank = i + 1, points = Points(i), s.gains, s.user, s.gamerTag, s.xuid, s.lastScore, s.newScore });
+                                      .Select((s, i) => new { rank = i + 1, points = WeeklyPoints(i), s.gains, s.user, s.gamerTag, s.xuid, s.lastScore, s.newScore });
 
                 var grouped = ranked.GroupBy(s => s.gains)
                                     .OrderByDescending(g => g.Key)
                                     .Select((s, i) => new { grouprank = s.Min(ss => ss.rank), grouppoints = s.Max(ss => ss.points), group = s.Select(i => i) })
-                                    .SelectMany(g => g.group.Select(u => new { rank = g.grouprank, u, points = g.grouppoints }));
+                                    .SelectMany(g => g.group.Select(u => new { rank = g.grouprank, u, points = g.grouppoints }))
+                                    .ToArray();
 
-                // writing output file
+                // writing output
+                // 1. first one in input csv format for the scanner itself or for excel 
 
-                var toText = grouped.Select(g => $"|{g.rank}.|{g.u.user}|{g.u.gamerTag}|{g.u.xuid}|{g.u.lastScore}|{g.u.newScore}|{g.u.gains}|{g.points}|");
+                var toNextWeek = grouped.Select(g => string.Join(CsvSeparator,
+                                                                 g.rank, g.u.user, g.u.gamerTag, g.u.xuid, g.u.lastScore, g.u.newScore, g.u.gains, g.points));
+                await File.WriteAllLinesAsync(args[1], CsvHeader.Concat(toNextWeek));
 
-                var output = new string[] {
-                                 "|#|User|Gamertag|XUID|Initial GS|Final GS|Gains|Points|",
-                                 "| --- | --- | --- | --- | --- | --- | --- | --- |"
-                             }
-                             .Concat(toText);
+                // 2. second one in Discourse table format for copy/pasting it to a forum post
 
-                await File.WriteAllLinesAsync(args[1], output);
+                var toDiscourse = grouped.Select(g => $"|{g.rank}.|{(g.rank < 11 ? '@': ' ')}{g.u.user}|{g.u.gamerTag}|{g.u.lastScore}|{g.u.newScore}|{g.u.gains}|{g.points}|");
+
+                if(args.Length == 3)
+                {
+                    await File.WriteAllLinesAsync(args[2], DiscourseHeader.Concat(toDiscourse));
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine();
+                    foreach(var line in DiscourseHeader.Concat(toDiscourse))
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
             }
         }
 
         private static long Gains(long gamerscoreBefore, long gamerscoreNow) => Math.Max(0, gamerscoreNow - gamerscoreBefore);
 
-        private static int Points(int i) => Math.Max(0, 10 - i);
+        private static int WeeklyPoints(int rank) => Math.Max(0, 10 - rank);
 
         /// <summary>
-        /// calls open XBL api to get the gamerscore for a Xbox User Id
+        /// calls open XBL api to get the gamerscore for a Xbox User Id (XUID)
         /// </summary>
         private static async Task<long> ReadCurrentGamerScore(string gamerTag, long xuid)
         {
@@ -86,55 +119,54 @@ namespace XboxeraLeaderboard
 
             try
             {
-                using(var httpClient = new HttpClient())
+                using var httpClient = new HttpClient();
+
+                var request = new HttpRequestMessage
                 {
-                    var request = new HttpRequestMessage
-                    {
-                        Method = HttpMethod.Get,
-                        RequestUri = new Uri($"https://xbl.io/api/v2/account/{xuid}"),
-                    };
-                    request.Headers.Add("X-Authorization", OpenXBLKey);
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://xbl.io/api/v2/account/{xuid}"),
+                };
+                request.Headers.Add("X-Authorization", OpenXBLKey);
 
-                    var response = await httpClient.SendAsync(request);
+                var response = await httpClient.SendAsync(request);
 
-                    // response JSON looks like
-                    // {
-                    //   "profileUsers": [
-                    //   {
-                    //     "id": "2535413400000000",
-                    //     "hostId": "2535413400000000",
-                    //     "settings": [
-                    //       {
-                    //         "id": "GameDisplayPicRaw",
-                    //         "value": "http://images-eds.xboxlive.com/image?url=wHwbXKif8cus8csoZ03RW_ES.ojiJijNBGRVUbTnZKsoCCCkjlsEJrrMqDkYqs3MBhMLdvWFHLCswKMlApTSbzvES1cjEAVPrczatfOc0jR0Ss4zHEy6ErElLAY8rAVFRNqPmGHxiumHSE9tZRnlghsACzaoisWEww1VSUd9Sx0-&format=png"
-                    //         },
-                    //         {
-                    //         "id": "Gamerscore",
-                    //         "value": "6855"
-                    //         }, ...
+                // response JSON looks like
+                // {
+                //   "profileUsers": [
+                //   {
+                //     "id": "2535413400000000",
+                //     "hostId": "2535413400000000",
+                //     "settings": [
+                //       {
+                //         "id": "GameDisplayPicRaw",
+                //         "value": "http://images-eds.xboxlive.com/image?url=wHwbXKif8cus8csoZ03RW_ES.ojiJijNBGRVUbTnZKsoCCCkjlsEJrrMqDkYqs3MBhMLdvWFHLCswKMlApTSbzvES1cjEAVPrczatfOc0jR0Ss4zHEy6ErElLAY8rAVFRNqPmGHxiumHSE9tZRnlghsACzaoisWEww1VSUd9Sx0-&format=png"
+                //         },
+                //         {
+                //         "id": "Gamerscore",
+                //         "value": "6855"
+                //         }, ...
 
-                    if(response.IsSuccessStatusCode)
-                    {
-                        var content = await response.Content.ReadAsStringAsync();
+                if(response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
 
-                        var settings = JObject.Parse(content)["profileUsers"].First()["settings"].Children();
-                        var gamerscoreSettings = settings.Select(s => s.ToObject<OpenXblSettings>())
-                                                         .First(s => s.Id == "Gamerscore");
+                    var settings = JObject.Parse(content)["profileUsers"].First()["settings"].Children();
+                    var gamerscoreSettings = settings.Select(s => s.ToObject<OpenXblSettings>())
+                                                     .First(s => s.Id == "Gamerscore");
 
-                        // open XBL api only allows 10 requests per 15 seconds, so give it some time
-                        await Task.Delay(5000);
+                    // open XBL api only allows 10 requests per 15 seconds, so give it some time
+                    await Task.Delay(5000);
 
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("OK");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("OK");
 
-                        return long.Parse(gamerscoreSettings.Value);
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"error = {response.ReasonPhrase}");
-                        return 0;
-                    }
+                    return long.Parse(gamerscoreSettings.Value);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"error = {response.ReasonPhrase}");
+                    return 0;
                 }
             }
             catch(Exception exc)
